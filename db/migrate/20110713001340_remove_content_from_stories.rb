b9292e5dc8f5fbb426f605d73a42dcaa04118fe6
Kevin Quinn <kevin@kevin-laptop.(none)>
2011-07-12 21:05:48 -0400

    migration issues hopefully resolved

--- db/migrate/20110713001340_remove_content_from_stories.rb
+++
@@ -1,9 +0,0 @@
-class RemoveContentFromStories < ActiveRecord::Migration
-  def self.up
-    remove_column :stories, :content
-  end
-
-  def self.down
-    add_column :stories, :content
-  end
-end
