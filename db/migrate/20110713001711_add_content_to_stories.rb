8d39e1f1518d668fbae6e36c791d3208baf86855
Kevin Quinn <kevin@kevin-laptop.(none)>
2011-07-12 22:23:58 -0400

    this is getting really annoying, can't seem to fix this migration clusterfuck

--- db/migrate/20110713001711_add_content_to_stories.rb
+++
@@ -1,9 +0,0 @@
-class AddContentToStories < ActiveRecord::Migration
-  def self.up
-    add_column :stories, :content, :varchar, :limit => 10000
-  end
-
-  def self.down
-    remove_column :stories, :content, :limit => 10000
-  end
-end
--- db/migrate/20110713005546_change_column_content_in_stories_table.rb
+++
@@ -1,8 +0,0 @@
-class ChangeColumnContentInStoriesTable < ActiveRecord::Migration
-  def self.up
-    change_column :stories, :content, :text
-  end
-
-  def self.down
-  end
-end
